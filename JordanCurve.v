1d945bef872f3a628bf08d241bf0623efb02be80
Bradley Morrell <brrmorre@gmail.com>
2020-07-31 19:01:11 -0700

    Update JordanCurve.v

--- JordanCurve.v
+++ JordanCurve.v
@@ -1,6 +1,6 @@
 Definition SimpleClosedCurve := DedekindCuts.
 
-
+--Let SimpleClosedCurve be a simple closed curve in UnitCircle CartesianProduct UnitCircle. then SimpleClosedCurve separates UnitCircle CartesianProduct UnitCircle into two components Component1 and Component2. Each of the sets Component1 and Component2 has Boundary as its boundary; that is, Boundary equals Closure(Component1) without Component1 AND Boundary equals Closure(Component2) without Component2.
 Theorem JordanCurveTheorem : .
 Proof.
 
